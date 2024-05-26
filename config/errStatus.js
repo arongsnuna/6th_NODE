738b987d08b73ead7eddc9570cf9e2d92ce9065d
arongsnuna <unhi000@naver.com>
2024-05-26 21:00:49 +0900

    errStatus 수정

--- config/errStatus.js
+++ config/errStatus.js
@@ -1,11 +1,23 @@
 import { StatusCodes } from "http-status-codes";
 
 export const errStatus ={
-    // error
+    // common err
     INTERNAL_SERVER_ERROR: {status: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": "COMMON000", "message": "서버 에러, 관리자에게 문의 바랍니다." },
+    BAD_REQUEST: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "COMMON001", "message": "잘못된 요청입니다." },
+    UNAUTHORIZED: {status: StatusCodes.UNAUTHORIZED, "isSuccess": false, "code": "COMMON002", "message": "권한이 잘못되었습니다." },
+    METHOD_NOT_ALLOWED: {status: StatusCodes.METHOD_NOT_ALLOWED, "isSuccess": false, "code": "COMMON003", "message": "지원하지 않는 Http Method 입니다." },
+    FORBIDDEN: {status: StatusCodes.FORBIDDEN, "isSuccess": false, "code": "COMMON004", "message": "금지된 요청입니다." },
+    NOT_FOUND: {status: StatusCodes.NOT_FOUND, "isSuccess": false, "code": "COMMON005", "message": "요청한 페이지를 찾을 수 없습니다. 관리자에게 문의 바랍니다." },
 
     // member
     EMAIL_ALREADY_EXIST: {STATUS: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": "MEMBER2001", "message": "이미 존재하는 이메일입니다."},
     PARAMETER_IS_WRONG : {STATUS: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": "MEMBER2002", "message": "회원가입 잘못된 데이터를 입력했습니다."},
     LOGIN_FALSE: {STATUS: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": "MEMBER2003", "message": "로그인 실패합니다."},
+    // member err
+    MEMBER_NOT_FOUND: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4001", "message": "사용자가 없습니다."},
+    NICKNAME_NOT_EXIST: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4002", "message": "닉네임은 필수입니다."},
+
+    // article err
+    ARTICLE_NOT_FOUND: {status: StatusCodes.NOT_FOUND, "isSuccess": false, "code": "ARTICLE4001", "message": "게시글이 없습니다."}
+
 }
